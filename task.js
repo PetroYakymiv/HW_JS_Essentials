a18784d40e416b8bb804e4333a90e5fbccdf2c2c
Petro Yakymiv <petroyakymiv@gmail.com>
2019-07-28 19:53:45 +0300

    Update task.js

--- task.js
+++ task.js
@@ -1,14 +1,12 @@
 //1. A function which prints str after n seconds.
-function printTimeout(str, n) 
-{ 
+function printTimeout(str, n) { 
   setTimeout(() => console.log(str) , n * 1000);
 }
 printTimeout('hello', 10) // hello will appear after 10secs in console
 
 
 //2. A function which returns sum of all numbers from 1 to n using recursion.
-function sumAll(n) 
-{ 
+function sumAll(n) { 
  if (n != 1) {
         return n + sumAll(n - 1);
     }
@@ -33,7 +31,6 @@ function bombTimer(str, time) {
             clearInterval();
             console.log(str);
         }, (time + 1) * 1000);
-
     }
 }
 
@@ -41,8 +38,7 @@ bombTimer('Boooom', 3);
 
 
 // 4. A function which returns factorial of number using recursion.
-function factorial(n)
-{ 
+function factorial(n){ 
  if (n == 0) { return 1; }
   else {
     if (n != 1) {
@@ -56,8 +52,7 @@ console.log(factorial(5)); // 120
 
 
 // 6. A function which takes an array of numbers and maxNumber, the function returns new array with numbers not higher than maxNumber.
-function filterNumbers(arr, maxNumber) 
-{ 
+function filterNumbers(arr, maxNumber) { 
  return arr.filter(function (item) {
       return item <= maxNumber;
     });     
@@ -65,25 +60,11 @@ function filterNumbers(arr, maxNumber)
 filterNumbers([1, 4, 8, 1, 20], 5) // [1, 4, 1]
 
 
-//7. A function that returns object with min and max numbers from array of numbers.
-function minMax(arr) 
-{ 
-  /* your code */ 
-}
-minMax([1, 4, 8, 2, 20]) // { max: 20, min: 1 }
-
-
-//8. A function that returns average of numbers in array.
-function average(arr) 
-{
-  /* your code */
-}
-average([1,4,2]) // 2.33
-
-
 //9. A function which concats all first-nested arrays in one array (use reduce):
 function concatFirstNestedArrays(arr) 
 { 
-  /* your code */ 
-}
-concatFirstNestedArrays([[0, 1], [2, 3], [4, 5]]) // [0, 1, 2, 3, 4, 5]
+    return arr.reduce((a, b) => a.concat(b));
+ }
+  concatFirstNestedArrays([[0, 1], [2, 3], [4, 5]]) // [0, 1, 2, 3, 4, 5]
+
+
